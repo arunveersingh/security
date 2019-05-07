9b2287420804034dd48752b23adf2bf48ad7c8a8
SINGH Arunveer <arunveer.singh@soprasteria.com>
2019-05-07 15:43:31 +0530

    Provide AuthorizationServerConfigurerAdapter bean to configure multiple clients.

---
+++ springbootoauth2server/src/main/java/com/arunveersingh/springbootoauth2server/AuthorizationServerConfig.java
@@ -0,0 +1,46 @@
+package com.arunveersingh.springbootoauth2server;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+
+/**
+ * This bean turns off auto-configuration of authorization server - which means Spring Boot will not create a bean of
+ * AuthorizationServerConfigurerAdapter with default properties.
+ *
+ * One of the reason to provide custom AuthorizationServerConfigurerAdapter bean is it is possible to provide
+ * multiple clients.
+ *
+ * See @AuthorizationServerConfig{@link #configure(ClientDetailsServiceConfigurer)}
+ *
+ * Try, curl first-client:noonewilleverguess@localhost:8080/oauth/token -dgrant_type=client_credentials -dscope=resource:read
+ *
+ */
+@Configuration
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients
+                .inMemory()
+                .withClient("first-client")
+                .secret(passwordEncoder().encode("noonewilleverguess"))
+                .scopes("resource:read")
+                .authorizedGrantTypes("client_credentials");
+
+        clients
+                .inMemory()
+                .withClient("second-client")
+                .secret(passwordEncoder().encode("noonewilleverguess"))
+                .scopes("resource:read")
+                .authorizedGrantTypes("client_credentials");
+    }
+}
