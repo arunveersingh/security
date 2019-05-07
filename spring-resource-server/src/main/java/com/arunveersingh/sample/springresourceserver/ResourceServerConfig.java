8de6cd0e59ddbcdfc5c6243f318b2636ac34648b
arunveersingh <arunveersingh9@gmail.com>
2019-05-08 00:14:11 +0530

    Code to demo the integration of resource server with authorization server. Auth Server is running as a diff service.

--- spring-resource-server/src/main/java/com/arunveersingh/sample/springresourceserver/ResourceServerConfig.java
+++ spring-resource-server/src/main/java/com/arunveersingh/sample/springresourceserver/ResourceServerConfig.java
@@ -1,8 +1,27 @@
 package com.arunveersingh.sample.springresourceserver;
 
+import org.apache.catalina.filters.RequestDumperFilter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
 @Configuration
-public class ResourceServerConfig {
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/user/")
+                .access("#oauth2.hasScope('resource:read')");
+    }
+
+    @Bean
+    RequestDumperFilter requestDumperFilter() {
+        return new RequestDumperFilter();
+    }
 }
--- spring-resource-server/src/main/java/com/arunveersingh/sample/springresourceserver/SpringResourceServerApplication.java
+++ spring-resource-server/src/main/java/com/arunveersingh/sample/springresourceserver/SpringResourceServerApplication.java
@@ -2,8 +2,10 @@ package com.arunveersingh.sample.springresourceserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
+@EnableResourceServer
 public class SpringResourceServerApplication {
 
     public static void main(String[] args) {
